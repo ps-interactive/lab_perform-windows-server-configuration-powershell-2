5f2e721b5d37b77e9ab71ebb22129b95c1dbc1f6
Uzair Ansari <uzair_ansari3@hotmail.com>
2023-10-11 13:04:49 +0530

    Initial commit

---
+++ Windows_Lab_Script.ps1
@@ -0,0 +1,13 @@
+﻿# 1 - Create local user and group
+
+# 1.1.A - Get list of all local users
+Get-LocalUser
+
+# 1.1.B - List all the properties of Admin user
+Get-LocalUser -Name Administrator | select-object *
+
+# 1.1.C - Create a new user
+#Prompt to enter new user name
+$NewUser = Read-Host "New local admin username:"
+
+#
